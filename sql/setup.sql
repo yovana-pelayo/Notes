75f4a5c61d68e9ab1004231820671b207d08a64c
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-09-29 15:30:48 -0700

    established classes and created an insert function that takes data and stores in in correct columns

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,12 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE if exists users cascade;
+
+CREATE TABLE users (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    username VARCHAR NOT NULL UNIQUE,
+password_hash VARCHAR NOT NULL
+);
+
+INSERT INTO users (username, password_hash) VALUES ('yoyosworld', 'Christmastreeswhores')
+
